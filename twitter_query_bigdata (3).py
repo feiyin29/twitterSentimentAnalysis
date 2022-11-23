1ffc2bd8afb3bc748cf419a33a2bc245524ac550
feiyin29 <feiyinn29@gmail.com>
2022-11-23 12:51:20 +0700

    Clean Code and Add comment

--- twitter_query_bigdata (3).py
+++ twitter_query_bigdata (3).py
@@ -1,30 +1,23 @@
-# import base64
+# import Library
 import streamlit as st
 import streamlit.components.v1 as components
-
-# st.header("Hello World 👏")
-
-# st.write("This is my first app")
-
 from cleantext import clean
 import numpy as np
-
 import pymongo
 from pymongo import MongoClient
 import json
 import tweepy as tw
-# import twitter
 from pprint import pprint
 import pandas as pd
-# word cloud
 from textblob import TextBlob
 from wordcloud import WordCloud
 import re
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
+# Back-end
 
-# Keys and Access Tokens
+# Keys and Access Tokens from twiter api
 consumer_key = '77UdiVpfBQpFZDvxuPlgvMFHs'
 consumer_secret = 'AvycXqpzW6eMv0sjZA6nkLGwqOh4Gh0WRCId0JAKVnABgxP3gb'
 access_token = '1531544153275785216-ei4xlrXO8sKVb3QXbkE3Mm12o6W6Jx'
@@ -34,14 +27,12 @@ auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
-
+#Search a word from twitter
 def search_tweets(language, hashtag, number):
 
     # Define the search term and the date_since date as variables
     search_words = hashtag
-    #date_since = date
     new_search = search_words + " -filter:retweets"  # Do not get retweet of tweets
-
     # Collect tweets
     tweets = tw.Cursor(api.search_tweets, q=new_search,
                        lang=language).items(number)
@@ -54,31 +45,23 @@ def search_tweets(language, hashtag, number):
     # return Dataframe
     return tweet_df
 
-
+#default dataframe
 df = search_tweets('en', 'blackpink', 10)
 
-# df.sample(10)
-
 # clean data
-
 # create a function to clean the tweets
-
-
 def cleanTxt(text):
     text = re.sub(r'@[A-Za-z0-9]+', '', text)  # remove @
     text = re.sub(r'#', '', text)  # Remove the '#' symbol
     text = re.sub(r'RT[\s]+', '', text)  # remove RT
     text = re.sub(r'https?:\/\/\S+', '', text)  # remove hyper link
-    text = clean(text, no_emoji=True)
+    text = clean(text, no_emoji=True) # remove emoji
 
     return text
 
 
 # Cleaning the text
 df['text'] = df['text'].apply(cleanTxt)
-
-# Show the cleaned text
-# df
 
 # Connect MongoDB
 mongod_connect = "mongodb+srv://bigdata_tweets:bigdata@cluster0.hifh0z9.mongodb.net/?retryWrites=true&w=majority"
@@ -92,37 +75,36 @@ def query(df):
     # Cleaning the text
     df['text'] = df['text'].apply(cleanTxt)
 
+    #create and write json file
     with open('df.json', 'w') as f:
         f.write(df.to_json())
 
+    #Load json file for insert to DB
     with open('df.json') as file:
         file_data = json.load(file)
-
-
+    
+    #delete data in DB
     tweet_collection.delete_many({})
+
+    #insert data in json file to DB
     tweet_collection.insert_one(file_data)
 
+# call function query to query data
 query(df)
+
 # create  a function to get the subjectivity
-
-
 def getSubjectivity(text):
     return TextBlob(text).sentiment.subjectivity
 
 # create a function to get the polarity -> negative positive
-
-
 def getPolarity(text):
     return TextBlob(text).sentiment.polarity
-
 
 # create two new column
 df['Subjectivity'] = df['text'].apply(getSubjectivity)
 df['Polarity'] = df['text'].apply(getPolarity)
 
-# show the new dataframe with yhe new column
-# df
-
+# check polarity state
 def getAnalysis(score):
     if score < 0:
         return 'Negative'
@@ -131,12 +113,10 @@ def getAnalysis(score):
     else:
         return 'Positive'
 
-
+# call getAnalysis function to store in Dataframe
 df['Analysis'] = df['Polarity'].apply(getAnalysis)
 
-# show the dataframe
-# df
-
+# wordClound Generate and make it circle
 def wordCloundIMG(df):
   text = ' '.join([twts for twts in df['text']])
 
@@ -144,7 +124,6 @@ def wordCloundIMG(df):
 
   mask = (x - 150) ** 2 + (y - 150) ** 2 > 130 ** 2
   mask = 255 * mask.astype(int)
-
 
   wc = WordCloud(background_color="white", repeat=True, mask=mask)
   wc.generate(text)
@@ -154,44 +133,39 @@ def wordCloundIMG(df):
   ax.axis("off")
   st.pyplot(fig)
 
+# visualize to bar chart
 def showValueCounts():
 # show the value counts
-  data = df['Analysis'].value_counts()
+  df['Analysis'].value_counts()
 
 # plot and visualize the count
   fig,ax = plt.subplots()
-#   fig.title('sentiment Analysis')
-#   fig.xlabel('Sentiment')
-#   fig.ylabel('Counts')
   df['Analysis'].value_counts().plot(kind='bar')
-#   plt.show()
   st.pyplot(fig)
-  
-# queryDB(tweet_collection)
-# wordCloundIMG()
 
-  # -------------------- UI ver2 ------------------------
+# front-end
+# set page title and layout
 st.set_page_config(
-     page_icon="🧊",
      page_title='Word Cloud',
      layout="wide",
      initial_sidebar_state="expanded",
 )
 
 def main():
-    cs_body()
+    body()
     return None 
 
-def cs_body():
+def body():
     with st.container():
-        # header_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">Word Cloud</p>'
+        # set header title
         header_title = '<p style="font-family:sans-serif; font-size: 52px;">Word Cloud</p>'
         st.markdown(header_title, unsafe_allow_html=True)
         text = ""
-        # example
+
+        # example word 
         col1, col2, col3, col4, col5, col6= st.columns((0.3, 0.24, 0.27, 0.3, 0.24, 1))   
         with col1:
-            eaxm = '<p style=" font-weight: 600;font-size: 24px; padding-top: 23px;">example :</p>'
+            eaxm = '<p style=" font-weight: 600;font-size: 24px; padding-top: 23px;">example word :</p>'
             st.markdown(eaxm, unsafe_allow_html=True)
         with col2:
             if(st.button('Netflix')):
@@ -226,37 +200,18 @@ def cs_body():
             if(st.button('CHAEUNWOO')):
                 text = "CHAEUNWOO"
         
-        # Search + Picture
-        # left, right = st.columns((2,1))
-        # with left:
-        #     title = st.text_input('', '',placeholder="Search")
-        #     df = search_tweets('en', title, 100)
-        # with right:
-        #     wordCloundIMG(df)
-                
-            
-        # # Search box   
-        # search1, search2, search3 = st.columns((0.5,2,0.5))
-        # with search2:
-        #     title = st.text_input('', 'Search')
-
-        # df = search_tweets('en', title, 100)
-
-        # # Gen pic
-        # emp1, pic, emp2 = st.columns((1,2,1))
-        # with pic:
-        #     wordCloundIMG(df)
-        search1, search2, search3, search4 = st.columns((0.5,2,0.5,0.5))
-        with search2:
+        # search bar and button
+        _, search1, search2, _ = st.columns((0.5,2,0.5,0.5))
+        with search1:
             name = st.text_input('', text,placeholder="Search")
-        with search3:
+        with search2:
             if(st.button('Generate')):
                 result = name.title()
             df = search_tweets('en', name, 100)
             query(df)
        
-        # Gen pic
-        emp1, pic, emp2 = st.columns((1,2,1))
+        # Show wordcloud image and bar chart
+        _, pic, _ = st.columns((1,2,1))
         with pic:
             wordCloundIMG(df) 
             titleGraph = '<p style="font-family:sans-serif; font-size: 30px; padding-top: 30px;">Graph Analysis Sentiment</p>'
@@ -266,42 +221,22 @@ def cs_body():
             st.markdown(descGraph, unsafe_allow_html=True)
             showValueCounts() 
             
-        # form = st.form(key='my-form')
-        # name = form.text_input('', '',placeholder="Search")
-        # submit = form.form_submit_button('Submit')
-
-        # if submit:
-        #     st.write(f'hello {name}')
-            
-    # def dfClick():
-    #     df = search_tweets('en', title, 100)
-    #     wordCloundIMG(df)
-
-            
     return None
 
+# call main front-end
 if __name__ == '__main__':
     main()
 
-
-    
-# @st.experimental_memo
-# def get_img_as_base64(file):
-#     with open(file, "rb") as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# img = get_img_as_base64("bird.png")
-
-        
-
+# css     
 m = st.markdown("""
 <style>
+    /* set background */
     [data-testid="stAppViewContainer"]  {
         background-color: #EEFAFF;
         background-image: url("https://cdn.discordapp.com/attachments/1032648545511161897/1044582208700158045/bg_big.jpg");
         background-size: cover
     }
+    /* set button state */
     div.stButton > button:first-child {
         border-radius: 53px;
         color:#ffffff;
@@ -316,66 +251,23 @@ m = st.markdown("""
         margin-top: 25px;
         box-shadow: 0px 0px 0px 0px rgb(0,0,0);
     }
-    div.stButton > button:active {
-        border-radius: 53px;
-        color:#000000;
-        border-color: #000000;
-        padding: 10px 32px;
-        box-shadow: 0px 0px 0px 0px rgb(0,0,0);
-    }
     div.stButton > button:focus {
         border-radius: 53px;
-        color:#000000;
+        color:#252525;
         border-color: #252525;
-        border-width: 5px;
+        border-width: 3px;
         padding: 10px 32px;
         box-shadow: 0px 0px 0px 0px rgb(0,0,0);
     }
-    div.stButton > button:visited {
-        border-radius: 53px;
-        color:#000000;
-        border-color: #000000;
-        border-width: 19px;
-        padding: 10px 32px;
-        margin-top: 25px;
-        box-shadow: 0px 0px 0px 0px rgb(0,0,0);
-    }
-    
+    /* set style search bar */
     [data-baseweb="input"] {
         border-radius: 53px;
         border-style: solid;
-        border-width: 5px;
+        border-width: 1.5px;
     }
-    
-    
 </style>""", unsafe_allow_html=True)    
 
-
-# m = st.markdown("""
-# <style>
-#     div.stButton > button:first-child {
-#         color:#ffffff;
-#         border-radius: 53px;
-#     }
-#     div.stButton > button:hover {
-#         background-color: #000000;
-#         color:#ff0000;
-#         border-radius: 53px;
-#         }
-# </style>""", unsafe_allow_html=True)
-
-# st.markdown(
-#         """
-#     <style>
-#     .stButton>button {
-#         border-radius: 53px;
-#     }
-#     </style>
-#     """,
-#         unsafe_allow_html=True,
-#     )
-
-
+# set button color
 components.html(
     """
 <script>
